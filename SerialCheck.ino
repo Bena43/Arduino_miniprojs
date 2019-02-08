6929f72a1d31bd3825766de543aabc0cfee8a82c
Bena43 <38659799+Bena43@users.noreply.github.com>
2019-02-08 23:33:41 +0900

    Mainly added comments
    made the variables name more indicative as well

--- SerialCheck.ino
+++ SerialCheck.ino
@@ -1,17 +1,15 @@
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
-pinMode(13,OUTPUT);
-
+Serial.begin(9600); // set the baud rate as 9600
+pinMode(13,OUTPUT); // set the 13th pin as an output pin
 }
-int Led_state;
-void loop() {
-  // put your main code here, to run repeatedly:
-if (Serial.available()>0)
-  Led_state = Serial.read();
-if (Led_state == '1')
-  digitalWrite(13,HIGH);
-if (Led_state == '0')
-  digitalWrite(13,LOW);
 
+int Serial_state; // global variable that recieves the commands from the serial
+
+void loop() {
+if (Serial.available()>0)       // as long as the serial works,
+  Serial_state = Serial.read(); // set Serial_state as the input that is recieved from the serial. 
+if (Serial_state == '1')        // if serial recieves 1 as an input
+  digitalWrite(13,HIGH);        // turn on the led
+if (Serial_state == '0')        // if serial recieves 0 as an input
+  digitalWrite(13,LOW);         // turn of the light
 }
