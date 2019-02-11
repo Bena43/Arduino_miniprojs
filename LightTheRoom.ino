66d1bc02869ed1bf6ca5f0f1f46ee2908ed644e3
Bena43 <38659799+Bena43@users.noreply.github.com>
2019-02-11 13:02:03 +0900

    Add files via upload

---
+++ LightTheRoom.ino
@@ -0,0 +1,30 @@
+/* 
+   Light the room
+   This program is turning on the LED on the board, as long as the room is dark.
+
+   compunents:
+   LED, Photoresistor, 6 wires, 220 ohm resistor, 10k ohm resistor, breadboard, Arduino Uno.
+
+   Created by Yehonatan Ben-Harush February 2019.
+ */
+
+
+//Variables
+int THvalue;          // Store threshold value from photoresistor (0-1023)
+
+void setup(){
+  Serial.begin(9600); // Set the serial
+ pinMode(6, OUTPUT);  // Set pin 6 as an output for the LED
+ pinMode(A0, INPUT);// Set pin A0 as an input for the photoresistor
+}
+
+void loop(){
+  THvalue = analogRead(A0);
+  Serial.println(THvalue); // Print the value of the photoresistor to the serial in order to set the thershod value
+  if (THvalue > 700){
+    digitalWrite(6, LOW);  //if the room is light, turn led off
+  }
+  else{
+    digitalWrite(6, HIGH); //if the room is dark, turn led on
+  }
+ }
